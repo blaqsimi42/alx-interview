af38b39a4baa0bf46d8ff6f5ada583679acce31d
blaqsimi42 <cimee464@gmail.com>
2024-10-17 13:47:50 +0000

    0x02-minimum_operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This funtion calculates the  min operations to copy and paste letters
+
+"""
+
+
+def minOperations(n):
+    nOpe = 0
+    minOpe = 2
+    while n > 1:
+        while n % minOpe == 0:
+            nOpe += minOpe
+            n /= minOpe
+        minOpe += 1
+    return nOpe
