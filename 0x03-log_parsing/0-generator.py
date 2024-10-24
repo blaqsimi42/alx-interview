5535816e40b224229da1f0519454accfb8712abf
blaqsimi42 <cimee464@gmail.com>
2024-10-24 23:52:35 +0000

    0-generator.py

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,7 +1,3 @@
-'''
-Beginning of code
-
-'''
 #!/usr/bin/python3
 import random
 import sys
@@ -17,9 +13,3 @@ for i in range(10000):
         random.randint(1, 1024)
     ))
     sys.stdout.flush()
-
-    '''
-    end of code
-    
-    '''
-
